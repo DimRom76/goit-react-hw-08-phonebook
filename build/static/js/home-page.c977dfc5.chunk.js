22c84c44950ab96b9a284aac252f5da84d2100d5
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-03-05 13:33:00 +0200

    deploy

---
+++ build/static/js/home-page.c977dfc5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[2],{349:function(e,t,c){"use strict";c.r(t),c.d(t,"default",(function(){return r}));var n=c(3),o=(c(0),c(121));function r(){return Object(n.jsx)("div",{className:"container",children:Object(n.jsx)(o.a,{className:"paper",children:Object(n.jsx)("h1",{children:"\u0413\u043b\u0430\u0432\u043d\u0430\u044f"})})})}}}]);
+//# sourceMappingURL=home-page.c977dfc5.chunk.js.map
